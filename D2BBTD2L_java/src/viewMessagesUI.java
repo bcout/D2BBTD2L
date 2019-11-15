25986cdb74e1df057f3c7edbfb203dfe04c620d1
Brennan Couturier <bcouturi@id415m36.cs.unb.ca>
2019-11-14 20:48:02 -0400

    Created a driver to test requestMessages

--- D2BBTD2L_java/src/viewMessagesUI.java
+++ D2BBTD2L_java/src/viewMessagesUI.java
@@ -1,4 +1,5 @@
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  * 
@@ -17,6 +18,11 @@ public class viewMessagesUI {
 	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	private viewMessagesControl vmc;
+	
+	public viewMessagesUI()
+	{
+		vmc = new viewMessagesControl();
+	}
 
 	/** 
 	* <!-- begin-UML-doc -->
@@ -26,13 +32,29 @@ public class viewMessagesUI {
 	public void displayMessages(int userId) {
 		// begin-user-code
 		// TODO Auto-generated method stub
+		ArrayList<Message> messages = new ArrayList<Message>();
+		
 		try
 		{
-			vmc.getMessages(userId);
+			messages = vmc.getMessages(userId);
 		}
 		catch (SQLException e)
 		{
 			System.err.println(e.getMessage());
+		}
+		
+		if (messages.isEmpty())
+		{
+			System.out.println("No messages");
+		}
+		else
+		{
+			System.out.println("Messages");
+			for (int i = 0; i < messages.size(); i++)
+			{
+				int messageId = messages.get(i).getMessageId();
+				System.out.println(i + ": " + messageId);
+			}
 		}
 		
 		// end-user-code
