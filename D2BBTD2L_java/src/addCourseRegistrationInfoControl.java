01a402810684f3ae49a39acc20e40c5013cd8b0a
Ben Donkin <benjamindonkin@gmail.com>
2019-11-24 00:02:19 -0400

    Reformatting to prepared statements
    Attempted to reformat the data manager to use prepared statements but
    discovered an error in the creation of the CourseRegistration table.
    Waiting to update the database until future implementing is complete.

--- D2BBTD2L_java/src/addCourseRegistrationInfoControl.java
+++ D2BBTD2L_java/src/addCourseRegistrationInfoControl.java
@@ -20,6 +20,9 @@ public class addCourseRegistrationInfoControl {
 	* @generated "UML to Java (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
 	*/
 	public boolean submitCourseRegistrationInfo(ArrayList<CourseRegistration> cr) {
+		if (cr.size() == 0) {
+			return false;
+		}
 		return dataManager.addCourseRegistrationInfo(cr);
 	}
 }
