2c0793c948cb133537cae77cb89706517c612707
Ben Donkin <benjamindonkin@gmail.com>
2019-11-25 23:19:49 -0400

    Committing before pulling from master

--- D2BBTD2L_java/src/DataManager.java
+++ D2BBTD2L_java/src/DataManager.java
@@ -821,7 +821,7 @@ public class DataManager{
 			}
 			
 			//execute the sql statement
-			pst.execute();
+			pst.executeUpdate();
 			return true;
 		} catch (SQLException e) {
 			System.err.println(e.toString());
